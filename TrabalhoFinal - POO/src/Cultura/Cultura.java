27aecdced83a6739d6aeb0a182ae74410901bf14
Fernando Engelmann Kunst <fernandoekunst@gmail.com>
2025-11-08 17:47:25 -0300

    realização do pacote Cultura

--- TrabalhoFinal - POO/src/Cultura/Cultura.java
+++ TrabalhoFinal - POO/src/Cultura/Cultura.java
@@ -1,4 +1,52 @@
-package Cultura;
+package cultura;
 
 public class Cultura {
+
+    private int id;
+    private String nome;
+    private FaseCultura faseCrescimento;
+    private double necessidadeHidrica;
+    private double producaoEstimativa;
+
+    public Cultura(int id, String nome, FaseCultura faseCrescimento,
+                   double necessidadeHidrica, double producaoEstimativa) {
+        this.id = id;
+        this.nome = nome;
+        this.faseCrescimento = faseCrescimento;
+        this.necessidadeHidrica = necessidadeHidrica;
+        this.producaoEstimativa = producaoEstimativa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public FaseCultura getFaseCrescimento() {
+        return faseCrescimento;
+    }
+
+    public double getNecessidadeHidrica() {
+        return necessidadeHidrica;
+    }
+
+    public double getProducaoEstimativa() {
+        return producaoEstimativa;
+    }
+
+    public void atualizarFaseCrescimento(FaseCultura novaFase) {
+        this.faseCrescimento = novaFase;
+    }
+
+    public double calcularProdutividade() {
+        return producaoEstimativa;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (fase: " + faseCrescimento + ")";
+    }
 }
--- TrabalhoFinal - POO/src/Cultura/FaseCultura.java
+++ TrabalhoFinal - POO/src/Cultura/FaseCultura.java
@@ -1,4 +1,8 @@
 package Cultura;
 
 public enum FaseCultura {
+    GERMINACAO,
+    CRESCIMENTO,
+    FLORACAO,
+    COLHEITA
 }
